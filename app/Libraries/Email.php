58a803f375da8179b9b935a3128f9e8b18401000
Duc Pham <ducpminh668@gmail.com>
2021-07-25 14:25:46 +0700

    initial project

---
+++ app/Libraries/Email.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Libraries;
+
+
+use App\Exceptions\GeneralException;
+use App\Helpers\Mail\SetMailConfig;
+use Illuminate\Support\Facades\Mail;
+
+class Email
+{
+    public function sendEmail($mailText, $email, $subject = null, $fileNameToStore = null)
+    {
+        (new SetMailConfig)->set();
+
+        try {
+
+            Mail::send([], [], function ($message) use ($email, $subject, $mailText, $fileNameToStore) {
+
+                $mailer = $message->to($email)
+                    ->subject($subject)
+                    ->setBody($mailText, 'text/html');
+
+                if ($fileNameToStore) {
+                    $pdf =  'uploads/pdf/' . $fileNameToStore;
+                    $mailer->Attach(\Swift_Attachment::fromPath($pdf));
+                }
+
+                return $mailer;
+            });
+
+            return true;
+
+        } catch (\Exception $e) {
+            throw new GeneralException($e->getMessage(), 401);
+            // throw new GeneralException(trans('default.something_wrong_with_your_email_settings'), 401);
+        }
+
+    }
+
+}
